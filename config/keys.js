7a0f54a1c7d655343aab0500ace8295f1a605e95
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-30 15:02:27 +0000

    Creating Google OAuth Credentials

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://jolanta:jolanta@ds231987.mlab.com:31987/storybooks-dev',
+  googleClientID: '904695917627-4dvlteq4oh1nvov5937e1u5anfdsdgtn.apps.googleusercontent.com',
+  googleClientSecret: '9am5PwTYOeB20qu2yAIwDMUa'
+}
