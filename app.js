56dbf31521488077b2e1b0e9a01400602f46f73d
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-30 14:37:15 +0000

    Port fix

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-var port = process.env.PORT || 5000;
-
-app.listen(function(port){
-  console.log(`Server statrted on ort ${port}`)
+app.get('/', function(req, res){
+  res.send('it Works');
 });
+
+var port = process.env.PORT || 5000
+
+app.listen(port, function(){
+  console.log(`Server statrted on port ${port}`)
+});
+
+module.exports = app;
