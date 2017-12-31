41b9e97713d8ec606e6870bf117996a2541a9b88
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-31 15:52:10 +0000

    Verify and Logout routes set up

--- routes/auth.js
+++ routes/auth.js
@@ -11,4 +11,18 @@ router.get('/google/callback',
     res.redirect('/dashboard');
   });
 
+router.get('/verify', function(req, res){
+  if(req.user){
+    console.log(req.user);
+  } else {
+    console.log('Not Auth');
+  }
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+
+});
+
 module.exports = router;
