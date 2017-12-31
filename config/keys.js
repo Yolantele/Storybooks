46c4cfdcb78fd24356a6dc1b092baf0e847de627
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-12-31 14:22:52 +0000

    Fixes google Client ID and Secret keys for Oauth

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:'mongodb://jolanta:jolanta@ds231987.mlab.com:31987/storybooks-dev',
-  googleClientID: '904695917627-4dvlteq4oh1nvov5937e1u5anfdsdgtn.apps.googleusercontent.com',
-  googleClientSecret: '9am5PwTYOeB20qu2yAIwDMUa'
+  googleClientID: '904695917627-t4dodo0l2pj7t9a0c8c242cp10rr8vf3.apps.googleusercontent.com',
+  googleClientSecret: 'QroxOa7PLpsIqpdh5-xqF3BK'
 }
